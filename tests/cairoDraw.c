19db1d1885c36e13b0b2a0cf0b45a2b307ef5181
Lance Arsenault <lanceman@vt.edu>
2024-09-26 14:04:38 -0400

    Saving Work

--- tests/cairoDraw.c
+++ tests/cairoDraw.c
@@ -21,9 +21,30 @@ int draw(struct SlWindow *win, uint32_t *pixels,
             (unsigned char *) pixels, CAIRO_FORMAT_ARGB32,
             w, h, stride);
 
+#if 0
+    const int NUM = 20;
 
+    cairo_t *c[NUM];
+    for(int i = 0; i<NUM; ++i) {
+        c[i] = cairo_create(surface);
+        ERROR("cr=%p", c[i]);
+        ASSERT(c[i]);
+    }
+
+    for(int i = 0; i<NUM; ++i)
+        cairo_destroy(c[i]);
+#endif
+
+    cairo_t *cr = cairo_create(surface);
+ERROR("cr=%p", cr);
     cairo_surface_destroy(surface);
+    cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
 
+    cairo_set_source_rgba(cr, 0, 0.9, 0, 0.4);
+
+    cairo_paint(cr);
+
+    cairo_destroy(cr);
 
     return 1; // stop calling
 
@@ -49,13 +70,30 @@ int main(void) {
 
     //slWindow_setDraw(win, draw);
 
+#ifdef LOOP
     while(slDisplay_dispatch(d));
-
+#endif
 
     // Use automatic cleanup from the libslate.so destructor.
 
     // If we make it here it does not seem to crash.
     DSPEW("DONE");
+
+
+    // This is fucking AWESOME.  Cairo has a robust memory cleanup method:
+    // in the function cairo_debug_reset_static_data().  I was working on
+    // a patch to fix the memory leaks in libcairo.so and came upon this
+    // after a half day of working.
+    //
+    // After adding this function call this program passes the Valgrind
+    // memory allocate/free test.  It is only safe to call this function
+    // when there are no active cairo objects remaining.  One would think
+    // it would figure out what to do, otherwise what's the point of
+    // having a computer.  I just have to cleanup my cairo code myself,
+    // before calling this; not like I did with the libslate.so library
+    // destructor (which cleans all things slate).
+    //
+    cairo_debug_reset_static_data();
 
     return 0;
 }
